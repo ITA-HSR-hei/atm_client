9a6adfe2deef1ba375d0febcff80796ceaa61526
rstaehli <github@arisen.ch>
2013-05-15 12:41:52 +0200

    fix logfile directory

--- administration/soundLevelStart.sh
+++ administration/soundLevelStart.sh
@@ -7,4 +7,5 @@ echo -n "Date: " >> $START_LOG
 date >> $START_LOG
 sudo python3.2 /home/pi/soundLevelMeter/soundLevelMeter.py >> $START_LOG 2>&1 &
 
+sleep 5
 echo --------------------------------------------------- >> $START_LOG
