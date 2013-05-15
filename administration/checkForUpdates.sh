6b8abb96f72196efe57b4f19b3e178e2b5f32264
rstaehli <github@arisen.ch>
2013-05-15 11:46:37 +0200

    changed checkForUpdates script

--- administration/checkForUpdates.sh
+++ administration/checkForUpdates.sh
@@ -4,21 +4,23 @@
 # 0 * * * * /home/pi/soundLevelMeter/administration/checkForUpdates.sh &>> /home/pi/soundLevelMeter/logs/checkForUpdates.log
 
 echo ""
-echo ------------Check For Updates------------
+echo ---------------Check For Updates---------------
 cd /home/pi/soundLevelMeter
+
+echo -n "Date: "cd ..
+date
+
 upToDate=$(sudo git remote show origin | grep "local out of date")
 
 if [[ -z "$upToDate" ]]; then
-        echo "No changes - SoundLevelMeter is up to date"
+        echo "INFO: No changes - SoundLevelMeter is up to date"
 else
-        echo "Changes available - Fetsch new data and restart"
+        echo "INFO: Changes available - Fetsch new data and restart"
         sudo git fetch --all
         sudo git reset --hard origin/master
-        
-		chmod +x administration/checkForUpdates.sh
 		
 		echo "INFO: Restart in 10 seconds"
 		sleep 10
 		sudo reboot -f
 fi
-echo -----------------------------------------
+echo -----------------------------------------------
