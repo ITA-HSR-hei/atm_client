20005c3122ed5e87cc74deb1bd530e4a7818e1ed
rstaehli <github@arisen.ch>
2013-05-15 14:31:57 +0200

    added script to startup routine

--- administration/installSoundLevelMeter.sh
+++ administration/installSoundLevelMeter.sh
@@ -3,16 +3,21 @@
 # 1. Adds crontab for which checks for script updates
 # 2. Adds soundLevelMeter Script to startup
 
-
-installSoundLevelMeter
+SOUND_LEVEL_HOME=/home/pi/soundLevelMeter
 WHO_AM_I=$(whoami)
+
+echo "INFO: Start install script"
+sleep 1
+
+
 if [[ "$WHO_AM_I" != "root"]]; then
 	echo "You must be root to install soundLevelMeter"
 	exit 1
 fi
 
-# Remove and add crontab
-CRON_ENTRY="* * * * * /home/pi/soundLevelMeter/administration/checkForUpdates.sh >> /home/pi/soundLevelMeter/logs/checkForUpdates.log 2>&1"
+echo "INFO: Add cronjob which checks for updates"
+
+CRON_ENTRY="* * * * * ${SOUND_LEVEL_HOME}/administration/checkForUpdates.sh >> ${SOUND_LEVEL_HOME}/logs/checkForUpdates.log 2>&1"
 TEMP_FILE=/tmp/temp_crontab_entries.txt
 
 crontab -l -u root | grep -v "checkForUpdates.sh" > $TEMP_FILE
@@ -20,3 +25,8 @@ echo $CRON_ENTRY >> $TEMP_FILE
 crontab -u root $TEMP_FILE
 
 
+echo -n "INFO: add soundLevelMeter script to startup routine"
+cp "${SOUND_LEVEL_HOME}/administration/soundLevelMeterStart.sh" /etc/init.d
+update-rc.d /etc/init.d/soundLevelMeterStart.sh defaults 
+
+echo -n "INFO: Finished install script"
