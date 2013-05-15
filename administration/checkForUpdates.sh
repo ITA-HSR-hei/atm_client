ce63773bf9c1f83af83d4c52952360e19bd7c519
rstaehli <github@arisen.ch>
2013-05-15 11:23:58 +0200

    added header

--- administration/checkForUpdates.sh
+++ administration/checkForUpdates.sh
@@ -3,14 +3,13 @@
 # sudo crontab -e
 # 0 * * * * /home/pi/soundLevelMeter/administration/checkForUpdates.sh &>> /home/pi/soundLevelMeter/logs/checkForUpdates.log
 
-
-
+echo ""
+echo ------------Check For Updates------------
 cd /home/pi/soundLevelMeter
 upToDate=$(sudo git remote show origin | grep "local out of date")
 
 if [[ -z "$upToDate" ]]; then
         echo "No changes - SoundLevelMeter is up to date"
-        exit 0
 else
         echo "Changes available - Fetsch new data and restart"
         sudo git fetch --all
@@ -22,3 +21,4 @@ else
 		sleep 10
 		sudo reboot -f
 fi
+echo -----------------------------------------
